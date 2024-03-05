bcb624027c8260195d80d97cc0d0216a754464b8
PopovaOlha <fc.odessa.10@gmail.com>
2024-03-05 18:39:30 +0300

    feat: add types and queries for user, profile and members

---
+++ src/routes/graphql/types/User.ts
@@ -0,0 +1,15 @@
+export type User = {
+    id: string;
+    name: string;
+    balance: number;
+    userSubscribedTo?: {
+      subscriberId: string;
+      authorId: string;
+    }[];
+    subscribedToUser?: {
+      subscriberId: string;
+      authorId: string;
+    }[];
+  };
+
+  
